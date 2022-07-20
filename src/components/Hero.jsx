9f791ee7f5eff7c34e24744ce75a9f59cd39ee36
Nicolas Orieta <orietasebastian@hotmail.com>
2022-07-20 00:55:57 -0300

    Hero agregado y cambio de logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+
 export const Hero = () => {
   return (
-    <div>
-        <h1 className='text-white'>hola</h1>
+    <div className='text-white'>
+        <div className='max-w-[800px] mx-auto text-center w-full h-[89vh] flex flex-col justify-center'>
+
+            <p className='md:p-0 uppercase font-bold text-[#00df9a] p-1'>crecer con análisis de datos</p>
+            <h1 className='md:text-6xl sm:text-6xl text-4xl font-bold md:py-3'>Crecer con datos</h1>
+
+            <div className='flex justify-center items-center'>
+              <p className='md:text-2xl sm:text-xl text-1xl font-bold'>Rapido, financiacion flexible para </p>
+              <p className=' md:text-3xl sm:text-xl text-1xl font-bold text-[#00df9a] pl-2 md:py-1 py-2'>BTB, BTC, SASS</p>
+            </div>
+
+            <p className='md:text-2xl text-xl font-bold text-gray-600 '>Supervise sus análisis de datos para aumentar los ingresos de las plataformas BTB, BTC, & SASS.</p>
+
+            <button className='bg-[#00df9a] w-[200px] rounded-md my-6 mx-auto py-3 text-black font-bold  '>Empezar</button>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Bambi<span className='text-white'>Data</span></h1>
         <ul className='hidden md:flex font-semibold'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Compania</li>
@@ -20,11 +20,11 @@ export const Navbar = () => {
             <li className='p-4'>Contacto</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+            {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full backdrop-blur-xl border-r border-r-gray-600 duration-500' : 'fixed left-[-100%] top-0 h-full w-[60%] duration-500'}>
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full backdrop-blur-xl border-r border-r-gray-600 duration-500' : 'fixed left-[-100%] top-0 h-full w-[60%] duration-500'}>
 
-            <h1 className='w-full text-3xl font-bold text-[#00df9a] mx-4 my-[30px]'>REACT.</h1>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a] mx-4 my-[30px]'>Bambi<span className='text-white'>Data</span></h1>
 
             <ul className='p-4 font-semibold'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
