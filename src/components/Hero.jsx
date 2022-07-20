263dd504b845d462e6bb8fb6625524be23b7a80b
Nicolas Orieta <orietasebastian@hotmail.com>
2022-07-20 12:37:16 -0300

    Efecto type  hero y redes en el nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Typewriter from 'typewriter-effect';
 
 export const Hero = () => {
   return (
@@ -10,8 +10,20 @@ export const Hero = () => {
             <h1 className='md:text-6xl sm:text-6xl text-4xl font-bold md:py-3'>Crecer con datos</h1>
 
             <div className='flex justify-center items-center'>
+
               <p className='md:text-2xl sm:text-xl text-1xl font-bold'>Rapido, financiacion flexible para </p>
-              <p className=' md:text-3xl sm:text-xl text-1xl font-bold text-[#00df9a] pl-2 md:py-1 py-2'>BTB, BTC, SASS</p>
+
+              <p className='md:text-3xl sm:text-xl text-1xl font-bold text-[#00df9a] pl-2 md:py-1 py-2'>
+              <Typewriter 
+                options={{ 
+                  strings: ['BTB', 'BTC', 'SASS'],
+                  autoStart: true,
+                  loop: true,
+                  pauseFor: 2000
+                }}
+              />
+              </p>
+
             </div>
 
             <p className='md:text-2xl text-xl font-bold text-gray-600 '>Supervise sus análisis de datos para aumentar los ingresos de las plataformas BTB, BTC, & SASS.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {AiOutlineMenu , AiOutlineClose } from 'react-icons/ai';
+import {AiOutlineMenu , AiOutlineClose, AiFillInstagram, AiFillLinkedin, AiOutlineFacebook } from 'react-icons/ai';
+import { FaGripLinesVertical } from 'react-icons/fa';
 
 export const Navbar = () => {
 
@@ -12,17 +13,21 @@ export const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Bambi<span className='text-white'>Data</span></h1>
+        
         <ul className='hidden md:flex font-semibold'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Compania</li>
             <li className='p-4'>Recursos</li>
             <li className='p-4'>Nosotros</li>
-            <li className='p-4'>Contacto</li>
+            <li className='p-4'>Conpmntacto</li>
         </ul>
+
+
         <div onClick={handleNav} className='block md:hidden'>
             {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20}/>}
         </div>
-        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full backdrop-blur-xl border-r border-r-gray-600 duration-500' : 'fixed left-[-100%] top-0 h-full w-[60%] duration-500'}>
+
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full backdrop-blur-xl border-r border-r-gray-600 duration-500 ' : 'fixed left-[-100%] top-0 h-full w-[60%] duration-500 '}>
 
             <h1 className='w-full text-3xl font-bold text-[#00df9a] mx-4 my-[30px]'>Bambi<span className='text-white'>Data</span></h1>
 
@@ -33,6 +38,17 @@ export const Navbar = () => {
                 <li className='p-4 border-b border-gray-600'>Nosotros</li>
                 <li className='p-4'>Contacto</li>
             </ul>
+
+            <div className='flex flex-col h-[45%] justify-end'>
+                <ul className='flex justify-center items-center py-3'>
+                    <li className='px-4 hover:text-orange-500 cursor-pointer'><AiFillInstagram size={20}/></li>
+                    <li><FaGripLinesVertical/></li>
+                    <li className='px-4 hover:text-blue-400 cursor-pointer'><AiFillLinkedin size={20}/></li>
+                    <li><FaGripLinesVertical/></li>
+                    <li className='px-4 hover:text-blue-800 cursor-pointer'><AiOutlineFacebook size={20}/></li>
+                </ul>
+                <p className='text-center font-medium text-xs'>BambiData® | 2022</p>
+            </div>
         </div>
     </div>
   )
